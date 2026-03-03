751e232828db7a919919d4ae7a11f5acc0a1276c
Corentin Dantinne <dantinne.corentin@gmail.com>
2026-03-03 14:23:27 +0100

    Routes - App

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,7 @@
 import express from "express";
+import AuthRoutes from "./routes/AuthRoute.js"
+import MissionRoutes from "./routes/MissionRoute.js"
+import ReviewRoutes from './routes/ReviewRoute.js'
 
 const app = express();
 
@@ -7,5 +10,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to the Missionly API !"});
 });
+
+app.use('/api/auth', AuthRoutes)
+app.use('/api/mission', MissionRoutes)
+app.use('/api/review', ReviewRoutes)
+
+
 
 export default app;
